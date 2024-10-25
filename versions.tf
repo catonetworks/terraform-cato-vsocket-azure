4a5f3fb841d2815a8162b56a1a43f2da044ce871
Brian Anderson <brian.anderson@catonetworks.com>
2024-10-25 14:10:23 -0700

    updating versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     cato = {
-      source = "catonetworks/cato"
+      source = "registry.terraform.io/catonetworks/cato"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
