26f49c9f899e90fb80f113cf75504ebffcfe41a5
Brian Anderson <brian.anderson@catonetworks.com>
2024-11-13 13:27:42 -0800

    added site_id and site_serial outputs

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,3 @@
 ##The following attributes are exported:
-
+output "socket_site_id" { value = cato_socket_site.azure-site.id }
+output "socket_site_serial" { value = data.cato_accountSnapshotSite.azure-site.info.sockets[0].serial }
